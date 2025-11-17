c18f9cf4323013748191f0f169e1b3a2d289e620
Manish Shah <Maneshkshah@gmail.com>
2025-11-16 22:16:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 echo "************************************************"
 echo "RUNNING"
 bash <(curl -sSL https://raw.githubusercontent.com/Nuzair46/BlockTheSpot-Mac/main/install.sh)
+bash <(curl -sSL https://raw.githubusercontent.com/SpotX-CLI/SpotX-Mac/main/install.sh)
